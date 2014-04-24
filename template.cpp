00a165d9e4eff7861391f4a1c7e9086bf4b67714
Nickolay Bukreyev <bnd95@pochta.ru>
2014-04-24 23:40:59 +0700

    'template.cpp' modified: * Headers added: <cctype>, <cfloat>, <ctime>, <deque>, <functional>, <numeric> * Headers are sorted in lexicographical order. * Typedef for 'ldouble' added. * Macro 'dbg(x)' removed. * Debug output stream added.

--- template.cpp
+++ template.cpp
@@ -1,54 +1,76 @@
+#define VERBOSE
 #define _USE_MATH_DEFINES
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <bitset>
-#include <list>
 #include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
 #include <iomanip>
-#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#ifndef _DEBUG
+#   undef VERBOSE
+#endif
+
+#define eps      1e-8
+#define inf      (1000 * 1000 * 1000)
+#define linf     (4LL * 1000 * 1000 * 1000 * 1000 * 1000 * 1000)
+#define sqr(x)   ((x) * (x))
+#define eq(x, y) (((x) > (y) ? (x) - (y) : (y) - (x)) <= eps)
+#define sz(x)    int((x).size())
+#define all(x)   (x).begin(), (x).end()
+#define rall(x)  (x).rbegin(), (x).rend()
+#define mp       make_pair
+
 using namespace std;
 
-typedef unsigned int uint;
-typedef long long llong;
+typedef unsigned           uint;
+typedef long long          llong;
 typedef unsigned long long ullong;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<double> vd;
-typedef vector<vd> vvd;
+typedef long double        ldouble;
+typedef vector<int>        vi;
+typedef vector<vi>         vvi;
+typedef vector<double>     vd;
+typedef vector<vd>         vvd;
 
-#define eps 1e-8
-#define inf 1000*1000*1000
-#define linf 4LL*1000*1000*1000*1000*1000*1000
-#define sqr(x) ((x)*(x))
-#define eq(x,y) ((((x)>(y))?(x)-(y):(y)-(x)) <= eps)
-#define sz(x) (int((x).size()))
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define mp(x,y) make_pair((x),(y))
-#define dbg(x) cout << #x << " : " << x << "\n"
+struct debug_t {
+    template <typename T>
+    debug_t& operator<<(const T& value) {
+#       ifdef VERBOSE
+            cout << value;
+#       endif
+        return *this;
+    }
+} debug;
+
+
 
 int main() {
 	//freopen(TASK ".in", "r", stdin);
 	//freopen(TASK ".out", "w", stdout);  
-	//freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
-	//freopen("info.txt","r",stdin);
-	//freopen("info.txt","w",stdout);
-	ios_base::sync_with_stdio(0);    
-	cin.tie(0);
-
-
-
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("info.txt", "r", stdin);
+	//freopen("info.txt", "w", stdout);
+	ios_base::sync_with_stdio(false);    
+	cin.tie(NULL);
+    
 	return 0;
 }
