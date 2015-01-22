9169a43e4bc117b796aad44b0fcc602cb217d58e
SirNickolas <bnd95@pochta.ru>
2015-01-23 05:03:13 +0700

    Updated template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,16 @@
+#define VERBOSE
 #define _USE_MATH_DEFINES
 #define _CRT_SECURE_NO_WARNINGS
 #include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <ctime>
 #include <cstring>
-#include <cmath>
+#include <ctime>
 #include <deque>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <list>
@@ -16,20 +20,27 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#ifdef LOCAL_PROJECT
+#   include <conio.h>
+#else
+#   undef VERBOSE
+#endif
+
+#define forn(i, n) for (int i = 0; i < (n); ++i)
+#define sqr(x)     ((x) * (x))
+#define eq(x, y)   (((x) > (y) ? (x) - (y) : (y) - (x)) <= eps)
+#define sz(x)      int((x).size())
+#define all(x)     (x).begin(), (x).end()
+#define rall(x)    (x).rbegin(), (x).rend()
+#define pb         push_back
+#define mp         make_pair
+#define eps        1e-8
+#define inf        int(1e9)
+#define linf       llong(4e18)
+
+#define TIMESTAMP(msg) cerr << "[" msg "] Time = " << clock() * 1.0 / CLOCKS_PER_SEC << '\n'
 
 using namespace std;
-
-#define forn(i,n) for(int i=0;i<(n);++i)
-#define pb        push_back
-#define mp        make_pair
-#define sz(x)     ((int)(x).size())
-#define eps       1e-8
-#define inf       (1000 * 1000 * 1000)
-#define linf      (4LL * 1000 * 1000 * 1000 * 1000 * 1000 * 1000)
-#define sqr(x)    ((x) * (x))
-#define eq(x, y)  (((x) > (y) ? (x) - (y) : (y) - (x)) <= eps)
-
-#define TIMESTAMP(msg) cerr<<"[" msg "] Time = "<<(clock()*1.0/CLOCKS_PER_SEC)<<"\n"
 
 typedef long long          llong;
 typedef unsigned long long ullong;
@@ -38,19 +49,32 @@ typedef vector<int>        vi;
 typedef vector<vi>         vvi;
 typedef vector<double>     vd;
 typedef vector<vd>         vvd;
-typedef pair<int,int>      pii;
+typedef pair<int, int>     pii;
+
+struct debug_t {
+    template <typename T>
+    debug_t& operator<<(const T& value) {
+#       ifdef VERBOSE
+            cout << value;
+#       endif
+        return *this;
+    }
+} debug;
+
+
 
 int main() {
 #   ifdef LOCAL_PROJECT
-        freopen("input.txt", "r", stdin);
-        //freopen("output.txt", "w", stdout);
-        //freopen(TASK ".in", "r", stdin);
-        //freopen(TASK ".out", "w", stdout);
-        //freopen("info.txt", "r", stdin);
-        //freopen("info.txt", "w", stdout);
+        freopen(".in", "r", stdin);
+#   else
+        freopen(".in", "r", stdin);
+        freopen(".out", "w", stdout);
 #   endif
-
-
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+#   ifdef LOCAL_PROJECT
+        _getch();
+#   endif
     return 0;
 }
