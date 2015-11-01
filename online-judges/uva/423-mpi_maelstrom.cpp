675fcaf25596b80a7b2508a7674fbf42ce53fc12
Ralor <zakharov.k.l@yandex.ru>
2015-11-01 10:36:49 +0600

    uva

--- online-judges/uva/423-mpi_maelstrom.cpp
+++ online-judges/uva/423-mpi_maelstrom.cpp
@@ -70,75 +70,41 @@ typedef vector<double>      vd;
 typedef vector<vd>          vvd;
 typedef pair<int,int>       pii;
 
-int n;
-string s;
-int dp[101][101];
-int dp2[100][1 << 16];
 
-int Dyn(int left, int right) {
-  if (right - left <= 1)
-    return right - left;
-  assert(left < right);
-  assert(left >= 0 && left < n);
-  assert(right > 0 && right <= n);
-  if (s[left] == s[right - 1])
-    return 0;
-  if (~dp[left][right])
-    return dp[left][right];
-  int result = INT_MAX;
-  for (int i = left + 1; i < right; i++)
-    result = min(result, Dyn(left, i) + Dyn(i, right));
-  return (dp[left][right] = result);
-}
-
-int Dyn2(int pos, int mask, int indent = 0) {
-  for (int j = 0; j < indent; j++)
-    debug << "    ";
-  debug << "-> " << pos _ bitset<15>(mask) << '\n';
-  if (pos == n)
-    return 0;
-  if (pos == n - 1)
-    return mask & 1 << (s[pos] - 'a') ? INT_MIN : 1;
-  if (~dp2[pos][mask])
-    return dp2[pos][mask];
-  char c = s[pos];
-  int result = INT_MIN;
-  bool ok = true;
-  int nextMask = mask | 1 << (s[pos] - 'a');
-  for (int i = pos + 1; i < n; i++)
-    if (s[i] == c) {
-      for (int j = 0; j <= indent; j++)
-        debug << "    ";
-      debug << "match\n";
-      int x = Dyn2(i + 1, mask, indent + 1);
-      result = max(result, x);
-      for (int j = 0; j <= indent; j++)
-        debug << "    ";
-      debug << "<- " << x _ result << '\n';
-      ok = false;
-    } else if (ok) {
-      nextMask |= 1 << (s[i - 1] - 'a');
-      if (nextMask & 1 << (s[i] - 'a')) {
-        ok = false;
-        continue;
-      }
-      int x = Dyn2(i, nextMask, indent + 1);
-      result = max(result, i - pos + x);
-      for (int j = 0; j <= indent; j++)
-        debug << "    ";
-      debug << "<- " << x _ result << '\n';
-    }
-  return (dp2[pos][mask] = result);
-}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  while (cin >> n >> s) {
-    memset(dp, 0xFF, sizeof dp);
-    memset(dp2, 0xFF, sizeof dp2);
-    cout << Dyn(0, n) _ Dyn2(0, 0x00) << '\n';
+  
+  int n;
+  while( cin>>n ) {
+    vvi a(n, vi(n, inf));
+    forn(u,n) {
+      string s;
+      forn(v,u) {
+        cin>>s;
+        if( s != "x" )
+          a[u][v] = a[v][u] = atoi(s.c_str());
+      }
+    }
+    vi dist(n, inf), used(n);
+    dist[0] = 0;
+    forn(iter, n) {
+      int u = -1;
+      forn(i,n) if( !used[i] && (u == -1 || (dist[u] > dist[i])) )
+        u = i;
+      if( dist[u] == inf )
+        break;
+      used[u] = 1;
+      forn(v,n) if( !used[v] && a[u][v] < inf ) 
+        dist[v] = min(dist[v], dist[u] + a[u][v]);
+    }
+    int result = 0;
+    forn(u,n)
+      if( dist[u] < inf ) result = max(result, dist[u]);
+    cout<<result<<"\n";
   }
+  
   TIMESTAMP("end");
   return 0;
 }
