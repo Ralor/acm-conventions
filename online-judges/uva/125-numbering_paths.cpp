a1395727960b64d7b1df5348710ee9718cffe989
Ralor <zakharov.k.l@yandex.ru>
2015-11-03 22:11:29 +0600

    uva apsp

---
+++ online-judges/uva/125-numbering_paths.cpp
@@ -0,0 +1,121 @@
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <valarray>
+#include <vector>
+#define forn(i, n) for (int i = 0; i < (n); ++i)
+#define sqr(x)     ((x) * (x))
+#define eq(x, y)   (((x) > (y) ? (x) - (y) : (y) - (x)) <= eps)
+#define sz(x)      int((x).size())
+#define all(x)     (x).begin(), (x).end()
+#define rall(x)    (x).rbegin(), (x).rend()
+#define fst        first
+#define snd        second
+#define pb         push_back
+#define mp         make_pair
+#define eb         emplace_back
+#define ef         emplace_front
+#define mt         make_tuple
+#define eps        1e-8
+#define inf        int(1e9)
+#define linf       llong(4e18)
+using namespace std;
+
+struct debug_t {
+  template<typename T>
+  debug_t& operator << (const T& o) {
+#   ifdef LOCAL_DEBUG
+    cout<<o<<flush;
+#   endif
+    return *this;
+  }
+} debug;
+
+#define TIMESTAMP(msg) debug << "[" msg "] Time = " << clock() * 1.0 / CLOCKS_PER_SEC << '\n' 
+#define DBG(x) debug << "[" #x "] = " << x << '\n'
+#define DOUT(x) debug << x << '\n'
+#define _ << " " <<
+
+typedef unsigned char       uchar;
+typedef basic_string<uchar> ustring;
+typedef long long           llong;
+typedef unsigned long long  ullong;
+typedef long double         ldouble;
+typedef vector<int>         vi;
+typedef vector<vi>          vvi;
+typedef vector<double>      vd;
+typedef vector<vd>          vvd;
+typedef pair<int,int>       pii;
+
+void mul(vvi& a, vvi& b, int n) {
+  static vvi t;
+  t.assign(n, vi(n));
+  forn(i,n) forn(j,n) {
+    forn(k,n)
+      t[i][j] += a[i][k] * b[k][j];
+  }
+  a = t;
+}
+void add(vvi& a, vvi& b, int n) {
+  forn(i,n) forn(j,n)
+    a[i][j] += b[i][j];
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int ci = 0;
+  
+  int m;
+  while( cin>>m ) {
+    vvi a(30, vi(30));
+    int n = 0;
+    forn(edge, m) {
+      int u,v; cin>>u>>v; a[u][v] = 1;
+      n = max({n,u+1,v+1});
+    }
+    vvi dp(n, vi(n)), temp = a;
+    forn(step, n) {
+      add(dp, a, n);
+      mul(a, temp, n);
+    }
+    a = temp;
+    forn(k, n) forn(u, n) if( a[u][k] ) forn(v, n) if( a[k][v] )
+      a[u][v] = min(inf, max(a[u][v], a[u][k] + a[k][v]));
+    forn(u, n) forn(v, n) forn(k, n)
+      if( a[u][k] && a[k][k] && a[k][v] )
+        a[u][v] = dp[u][v] = inf;
+    cout<<"matrix for city "<<ci++<<"\n";
+    forn(i, n) {
+      forn(j, n)
+        cout<<(j ? " " : "")<<( dp[i][j] == inf ? -1 : dp[i][j] );
+      cout<<"\n";
+    }
+  }
+  
+  TIMESTAMP("end");
+  return 0;
+}
